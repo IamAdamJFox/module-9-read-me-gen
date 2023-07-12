f96ed37807bedcbc91fce092dcfa483702984d62
adamjfox <adamjeffersonfox@gmail.com>
2023-07-12 15:44:58 -0700

    added and inquirer and start of questions

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "yes",
+    name: "yes",
+    message: "yes",
+    },
+    {
+    type: "yes",
+    name: "yes",
+    message: "yes",
+    },
+    {
+    type: "yes",
+    name: "yes",
+    message: "yes",
+    },
+];
+
+console.log(questions)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
