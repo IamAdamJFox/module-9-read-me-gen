49b66c964eb7883df7b2dc9d932baa13a1f766f0
adamjfox <adamjeffersonfox@gmail.com>
2023-07-12 23:18:32 -0700

    markdown mostly complete?

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,90 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  let badgeurl = '';
   if (license !== 'No liscense')
-  {badge = b}
-  return badge;
+  {badgeurl = "[![License](https://img.shields.io/badge/License-" + license + "-blue.svg)](https://opensource.org/licenses/" + license + ")"
+  }
+  return badgeurl;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === b) {
-    return ''
+  if (license === 'MIT') {
+    return 'https://lbesson.mit-license.org/'
   }
-  if (license === b) {
-    return ''
+  if (license === 'GPL') {
+    return 'https://www.gnu.org/licenses/gpl-3.0'
   }
-  if (license === b) {
-    return ''
+  if (license === 'IBM') {
+    return 'https://www.ibm.com/about/software-licensing/licensing/basics'
   }
-  if (license === b) {
-    return ''
+  if (license === 'CC0') {
+    return 'https://creativecommons.org/licenses/by-nd/4.0'
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'No License') {
+    return `## License
+
+This project is licensed under the ${license} license. Click [here](${renderLicenseLink(license)}) for more information.`
+  
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseBadge(data.License)}
-`;
+  ## Description
+
+${renderLicenseSection(data.license)}
+
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributers}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions about the repo, please contact me at 
+
+[${data.github}](https://github.com/${data.github})
+
+Thank you for your time.`;
 }
+
 
 module.exports = generateMarkdown;
 
-// (https://img.shields.io/badge/License-MIT-yellow.svg)
-// (https://opensource.org/licenses/MIT)
-// (https://img.shields.io/badge/License-Apache%202.0-blue.svg)
-// (https://opensource.org/licenses/Apache-2.0)
-// (https://img.shields.io/badge/License-GPLv3-blue.svg)
-// (https://www.gnu.org/licenses/gpl-3.0)
-// (https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)
-// (https://opensource.org/licenses/BSD-3-Clause)
+
